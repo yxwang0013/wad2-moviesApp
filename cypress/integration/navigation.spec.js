4966e2133a404992821ffe673daadfb560fcd5d6
yuxuan wang <2879296442@qq.com>
2020-11-05 20:20:49 +0000

    E2E tests - Navigation - Movies Details page Back button

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -75,4 +75,23 @@ describe("Navigation", () => {
       cy.get("h2").contains(movies[0].title);
     });
   });
+  describe("The Go Back button", () => {
+    beforeEach(() => {
+      cy.visit("/");
+    });
+    it("should navigate from home page to movie details and back", () => {
+      cy.get(".card").eq(1).find("img").click();
+      cy.get("svg[data-icon=arrow-circle-left]").click();
+      cy.url().should("not.include", `/movies`);
+      cy.get("h2").contains("No. Movies");
+    });
+    it("should navigate from favorites page to movie details and back", () => {
+      cy.get(".card").eq(0).find("button").click();
+      cy.get("nav").find("li").eq(1).find("a").click();
+      cy.get(".card").eq(0).find("img").click();
+      cy.url().should("not.include", `/favorites`);
+      cy.get("svg[data-icon=arrow-circle-left]").click();
+      cy.url().should("include", `/favorites`);
+    });
+  });
 });
