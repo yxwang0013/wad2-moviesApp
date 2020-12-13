c7527915dba8541161f12ac1db6ffbe5aa5ea18a
yuxuan wang <2879296442@qq.com>
2020-12-13 17:40:00 +0000

    nowplayingdetail

--- src/components/buttons/addToMovieDetail.js
+++ src/components/buttons/addToMovieDetail.js
@@ -1,21 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const addToMovieDetail = ({ movie }) => {
+const SeeDetails = ({ movie }) => {
   return (
     <Link
-    to={{
-      pathname: `/movies/${movie.id}`,
-      state: {
-        movie: movie
-      }
-    }}
-  >
-    Details
-  </Link>
-  
-    
+      className="btn w-100 btn-primary "
+      to={{
+        pathname: `/movies/${movie.id}`,
+        state: {
+          movie: movie
+        }
+      }}
+    >
+      Details
+    </Link>
   );
 };
 
-export default addToMovieDetail;
+export default SeeDetails;
