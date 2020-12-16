b5f3167e31a4bc873c1477a5815c9bcd56ac0c22
yuxuan wang <2879296442@qq.com>
2020-12-16 15:36:21 +0000

    add actor page test

---
+++ cypress/integration/actor-page.spec.js
@@ -0,0 +1,43 @@
+let stars
+let specifiedStar;
+
+describe('actor page', () => {
+    before(() => {
+        cy.request(
+            `https://api.themoviedb.org/3/person/popular?api_key=${Cypress.env(
+                "TMDB_KEY"
+            )}&language=en-US&page=1`
+        )
+            .its("body")
+            .then((response) => {
+                stars = response.results
+                specifiedStar = stars[0]
+            })
+    })
+    beforeEach(() => {
+        cy.visit("/")
+        cy.get("nav").find("li").eq(1).find("a").click();
+    });
+
+    describe("actor layout", () => {
+        it("should display page header", () => {
+            cy.get("h2").contains("Famous Actor");
+            cy.get(".badge").contains(20);
+        });
+        it('should render actor cards', () => {
+            cy.get(".card").should("have.length", stars.length);
+        })
+    })
+
+    describe('actor detail page', () => {
+
+        beforeEach(() => {
+            cy.get(".card").eq(0).click()
+        })
+
+        it('should render specfied actor', () => {
+            cy.get('h2').contains(specifiedStar.name);
+            cy.get('img').invoke('attr', 'src').should('contains', specifiedStar.profile_path)
+        })
+    })
+})
